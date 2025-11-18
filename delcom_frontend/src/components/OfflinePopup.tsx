17a61752301c81f9e1e8f908ac731bfc865deb7c
william-nyman <32524070+williamnyman@users.noreply.github.com>
2025-11-17 22:38:52 -0800

    online

--- delcom_frontend/src/components/OfflinePopup.tsx
+++ delcom_frontend/src/components/OfflinePopup.tsx
@@ -42,7 +42,7 @@ const OfflinePopup: React.FC = () => {
           }}
         >
           It will be live again by{" "}
-          <span style={{ fontWeight: "bold" }}>12pm PST Nov. 15</span>.
+          <span style={{ fontWeight: "bold" }}>9am PST Nov. 18</span>.
         </p>
       </div>
     </div>
